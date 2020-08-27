0056200ba544525010d7beba04e4fcb14579c7b6
Ben Hernandez <benjammin234@gmail.com>
2020-08-27 15:17:42 -0700

    Worked once

--- src/Login.js
+++ src/Login.js
@@ -2,38 +2,21 @@ import React from "react";
 import { GoogleLogin } from "react-google-login";
 
 export default class Login extends React.Component {
-  responseGoogle = (response) => {
-    console.log(response);
-  };
   render() {
+    const responseGoogle = (response) => {
+      console.log(response);
+    };
+
     return (
       <div className="Login">
         <form>
           <fieldset>
             <legend>Login</legend>
-            <label>
-              User name:{"  "}
-              <input
-                type="text"
-                name="user"
-                id="user"
-                aria-label="Enter your user name"
-                aria-required="true"
-                required
-              ></input>
-            </label>
-            <br />
-            <label>
-              Password:{"  "}
-              <input type="password" />
-            </label>
-            <div>
-              <button type="button">Login</button>
-            </div>
             <div>
               <GoogleLogin
-                onSuccess={this.responseGoogle()}
-                onFailure={this.responseGoogle()}
+                clientId="80581960464-bvpktqd6ocihkc8hlhe0ngup63vgq5ph.apps.googleusercontent.com"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
               />
             </div>
           </fieldset>
